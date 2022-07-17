df64ba16cc418dcfce5f0ce78097105ab168cf30
LukeG007 <luke@gilsinger.net>
2022-07-17 00:09:47 -0400

    Add self arg to functions in VNCServer class

--- process_mgmt.py
+++ process_mgmt.py
@@ -7,16 +7,16 @@ class VNCServer:
         self.user = user
         self.server_process = None
         self.novnc_process = None
-    def start_server_process():
+    def start_server_process(self):
         if self.debug:
             print('start_server_process called, port {}'.format(self.port))
         else:
             self.server_process = subprocess.Popen(['vncserver', '-rfbport', str(self.port-1)])
-    def start_novnc_process():
+    def start_novnc_process(self):
         if self.debug:
             print('start_novnc_process called, port {}'.format(self.port))
         else:
             self.novnc_process = subprocess.Popen(['./novnc/utils/novnc_proxy', '--listen', str(self.port), '--vnc', str(self.port-1)])
-    def start():
+    def start(self):
         self.start_novnc_process()
         self.start_server_process()
